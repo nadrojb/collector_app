9ccdc45f7f4993f562aba3b1c690f9e7196dd8c7
jordan_brewer <jordanbrewer1994@gmail.com>
2024-09-23 15:04:03 +0100

    function and foreach loop added to display each plant

---
+++ src/mycards.php
@@ -0,0 +1,52 @@
+<?php
+require_once 'connect.php';
+
+
+$db = connect();
+
+$query = $db -> prepare ('SELECT `name`, `growth_rate`,  `watering_needs`, `pet_friendliness`, `fertilising_needs` FROM `plants`');
+
+
+$result = $query->execute();
+
+if ($result){
+    $plants = $query -> fetchALL ();
+}else {
+    echo  'error';
+}
+
+
+function displayPlant($plants)
+{
+    foreach ($plants as $plant) {
+        $allPlants[] = "<h4>Name of Plant: {$plant['name']} </h4><br><p>Watering needs: {$plant['watering_needs']}</p><br><p>Growth rate: {$plant['growth_rate']}</p><br><p>Fertilising needs: Once {$plant['fertilising_needs']}</p><br><p>Pet friendliness: {$plant['pet_friendliness']}</p>";
+
+    }
+    return implode($allPlants);
+}
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+echo displayPlant($plants);
+
+?>
+
+
+
+
+</body>
+</html>
+
