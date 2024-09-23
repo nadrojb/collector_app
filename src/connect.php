a0b283b8aae8928a597392a0a920ef4548a3c109
jordan_brewer <jordanbrewer1994@gmail.com>
2024-09-23 11:09:16 +0100

    field name, photo added to plants table

--- src/connect.php
+++ src/connect.php
@@ -1,14 +1,41 @@
 <?php
 
 
+function connect()
+{
+    return $db = new PDO( //creating a variable
+        'mysql:host=DB;dbname=houseplant',  //DSN, make sure there are no spaces!!!
+        'root',  //username
+        'password'  //password
+    );
+}
+
+
+
 $db = new PDO( //creating a variable
     'mysql:host=DB;dbname=houseplant',  //DSN, make sure there are no spaces!!!
     'root',  //username
     'password'  //password
 );
 
-if ($db){
-    echo 'connected';
-} else {
-    echo 'error with connection';
+if (!$db){
+
+    echo 'error with database connection';
 }
+
+
+$db -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+$query = $db -> prepare ('SELECT * FROM `plants`');
+
+
+$result = $query->execute();
+
+if ($result){
+    $plants = $query -> fetchALL ();
+}else {
+    echo  'something went wrong';
+}
+
+echo '<pre>';
+var_dump($plants);
