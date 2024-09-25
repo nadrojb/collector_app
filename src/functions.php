e759e6ecb7688d5fd99df4acef7d278d702b1210
jordan_brewer <jordanbrewer1994@gmail.com>
2024-09-25 10:11:59 +0100

    styling complete

---
+++ src/functions.php
@@ -0,0 +1,51 @@
+<?php
+require_once 'connect.php';
+
+$db = connectToDatabase();
+
+function pullPlantsFromDatabase(PDO $db): array
+{
+    $query = $db->prepare('SELECT `plants`.`name`, `plants`.`growth_rate_id`, `plants`.`watering_needs`, `plants`.`pet_friendliness`, `plants`.`fertilising_needs`, `plants`.`photo`, `growth_rates`.`rate` FROM `plants` JOIN `growth_rates` ON `plants`.`growth_rate_id` = `growth_rates`.`id`');
+
+    $result = $query->execute();
+
+    if ($result) {
+        $plants = $query->fetchALL();
+        return $plants;
+    } else {
+        return [];
+    }
+}
+
+
+function displayPlants(array $plants): string
+{
+    $result = '';
+    foreach ($plants as $plant){
+
+    if ($plant['pet_friendliness'] === 1){
+        $petFriendly = 'Considered pet friendly';
+    }else{
+        $petFriendly= 'Not pet friendly';
+    }
+
+         $result .= "
+                     <div class='plant'>
+                     <h4 class='plant-name'>{$plant['name']} </h4> <br>
+                     <img src='{$plant['photo']}'>
+                     <div class='description'>
+                     <p class='margin-plant-description description-divider'>Watering needs are considered {$plant['watering_needs']}</p> <br>
+                     <p class='margin-plant-description'>This plant has a {$plant['rate']} growth rate</p> <br>
+                     <p class='margin-plant-description'>Fertilse {$plant['fertilising_needs']} during it's growing season</p> <br>
+                     <p class='margin-plant-description'>$petFriendly</p>                   
+                      </div>  
+                     </div>
+                    ";
+    }
+    return $result;
+}
+?>
+
+
+
+
